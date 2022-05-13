c7e20534a74964dc601e2851b568a4cd228ae995
Kelby Mah <kelbymah@gmail.com>
2022-05-13 13:03:54 -0600

    refactor signup page tests to use aliases

--- cypress/integration/mine/signUp.spec.ts
+++ cypress/integration/mine/signUp.spec.ts
@@ -15,24 +15,28 @@ describe("The Sign Up Page", () => {
   beforeEach(() => {
     cy.visit("/signup");
     cy.bypassLoading();
+    cy.get("[data-cy=signup-username-input]").as("signupUsernameInput");
+    cy.get("[data-cy=signup-password-input]").as("signupPasswordInput");
+    cy.get("[data-cy=signup-btn]").as("signupBtn");
   });
 
   it("displays sign up page with form", () => {
     cy.get("h3").should("have.text", "Sign Up");
     cy.contains("Existing account?");
-    cy.get("[data-cy=signup-username-input]").should("exist");
-    cy.get("[data-cy=signup-password-input]").should("exist");
-    cy.get("[data-cy=signup-btn]").should("be.disabled");
+    cy.get("@signupUsernameInput").should("exist");
+    cy.get("@signupPasswordInput").should("exist");
+    cy.get("@signupBtn").should("be.disabled");
   });
 
   it("entering invalid usernames shows appropriate validation errors", () => {
     // have to select child div then input in order to use focus and blur
-    cy.get("[data-cy=signup-username-input] > div > input").focus().blur();
+    cy.get("@signupUsernameInput").find("div > input").focus().blur();
     cy.contains("Required").should("exist");
-    cy.get("[data-cy=signup-username-input] > div > input").type("a").blur();
+    cy.get("@signupUsernameInput").find("div > input").type("a").blur();
     cy.contains("username must be between 3 - 50 characters").should("exist");
     // test 51 character string
-    cy.get("[data-cy=signup-username-input] > div > input")
+    cy.get("@signupUsernameInput")
+      .find("div > input")
       .clear()
       .type("Lorem ipsum dolor sit amet, consectetur porta ante.")
       .blur();
@@ -40,9 +44,9 @@ describe("The Sign Up Page", () => {
   });
 
   it("entering invalid passwords shows appropriate validation errors", () => {
-    cy.get("[data-cy=signup-password-input] > div > input").focus().blur();
+    cy.get("@signupPasswordInput").find("div > input").focus().blur();
     cy.contains("Required").should("exist");
-    cy.get("[data-cy=signup-password-input] > div > input").type("a").blur();
+    cy.get("@signupPasswordInput").find("div > input").type("a").blur();
     cy.contains("password must be longer than 3 characters").should("exist");
   });
 
@@ -61,9 +65,9 @@ describe("The Sign Up Page", () => {
     });
 
     it("succeeds when using a username that doesn't already exist", () => {
-      cy.get("[data-cy=signup-username-input]").type("newuser");
-      cy.get("[data-cy=signup-password-input]").type("password");
-      cy.get("[data-cy=signup-btn]").click();
+      cy.get("@signupUsernameInput").type("newuser");
+      cy.get("@signupPasswordInput").type("password");
+      cy.get("@signupBtn").click();
       cy.contains("Account created!").should("exist");
       // clicking proceed to login page goes to login page
       cy.get("[data-cy=proceed-login-btn]").should("exist").click();
@@ -74,9 +78,9 @@ describe("The Sign Up Page", () => {
     });
 
     it("fails when using a username that already exists", () => {
-      cy.get("[data-cy=signup-username-input]").type("existing");
-      cy.get("[data-cy=signup-password-input]").type("password");
-      cy.get("[data-cy=signup-btn]").click();
+      cy.get("@signupUsernameInput").type("existing");
+      cy.get("@signupPasswordInput").type("password");
+      cy.get("@signupBtn").click();
       cy.contains("username must be unique").should("exist");
       cy.contains("Account created!").should("not.exist");
     });
